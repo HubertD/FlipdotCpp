f406b926c9392dadb3ffbfd41b5a07de60811493
Hubert Denkmair <hubert@denkmair.de>
2016-10-02 22:31:18 +0200

    add SDL framebuffer implementation

---
+++ SDLFlipdotFramebuffer.cpp
@@ -0,0 +1,69 @@
+#include "SDLFlipdotFramebuffer.h"
+
+SDLFlipdotFramebuffer::SDLFlipdotFramebuffer(SDL_Window* window)
+  : _window(window), _renderer(nullptr)
+{
+	clear();
+}
+
+void SDLFlipdotFramebuffer::init()
+{
+	_renderer = SDL_CreateRenderer(_window, -1, 0);
+	clear();
+	flush();
+}
+
+void SDLFlipdotFramebuffer::flush()
+{
+	redraw();
+}
+
+void SDLFlipdotFramebuffer::update()
+{
+	redraw();
+}
+
+void SDLFlipdotFramebuffer::clear()
+{
+	for (unsigned y=0; y<NUM_PIXELS_Y; y++)
+	{
+		for (unsigned x=0; x<NUM_PIXELS_X; x++)
+		{
+			_buffer[y][x] = false;
+		}
+	}
+}
+
+void SDLFlipdotFramebuffer::setPixel(unsigned x, unsigned y, bool value)
+{
+	_buffer[y][x] = value;
+}
+
+void SDLFlipdotFramebuffer::redraw()
+{
+	if (_renderer == nullptr) {
+		return;
+	}
+
+	SDL_SetRenderDrawColor( _renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+	SDL_RenderClear(_renderer);
+
+	for (unsigned y=0; y<NUM_PIXELS_Y; y++)
+	{
+		for (unsigned x=0; x<NUM_PIXELS_X; x++)
+		{
+			SDL_Rect rect = {x*20, y*20, 20, 20};
+
+			if (_buffer[y][x]) {
+				SDL_SetRenderDrawColor(_renderer, 0x00, 0x00, 0x00, 0xFF);
+			} else {
+				SDL_SetRenderDrawColor(_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			}
+
+			SDL_RenderFillRect(_renderer, &rect );
+		}
+	}
+
+	SDL_RenderPresent(_renderer);
+}
+
